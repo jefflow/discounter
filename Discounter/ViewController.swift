57f593812e306c7841d0ffbd16282bbb896aedab
Jeff Low <jefflow1671@gmail.com>
2020-02-25 18:04:44 -0800

    Initial Commit

---
+++ Discounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Discounter
+//
+//  Created by Jeff Low on 2/25/20.
+//  Copyright © 2020 Jeff Low. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
